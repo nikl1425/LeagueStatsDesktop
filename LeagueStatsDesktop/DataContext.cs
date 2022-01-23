3b512b1213e8a278ac790c53a11a8efb3b0dbe91
niklas@astne.dk <niklas@astne.dk>
2022-01-23 18:30:23 +0100

    page1 on the way

---
+++ LeagueStatsDesktop/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LeagueStatsDesktop
+{
+    static class DataContext
+    {
+        private static readonly string _filesPath = "Files";
+
+        public static string FilesPath { get { return _filesPath; } }
+
+        public static Boolean PathExist(string pathToDir, out Boolean x)
+        {
+            x = Directory.Exists(pathToDir) ? true : false;
+            return x;
+        }
+
+        public static Boolean FileExist(string dir, string filepath, out Boolean x)
+        {
+            x = File.Exists($"{dir}/{filepath}") ? true : false;
+            return x;
+        }
+
+    }
+}
--- LeagueStatsDesktop/Views/Page1.xaml.cs
+++ LeagueStatsDesktop/Views/Page1.xaml.cs
@@ -24,5 +24,15 @@ namespace LeagueStatsDesktop.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Data_Check(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
